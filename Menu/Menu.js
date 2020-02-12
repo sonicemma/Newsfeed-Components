bf3d1b09ed9e07baa25bbcddcef431050a7a5aec
emma <sonicjane2020@gmail.com>
2020-02-12 13:50:13 -0800

    Finished with menu.js, met MVP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -21,17 +21,37 @@ let menuItems = [
 
 // The function takes an array as its only argument. 
 
+function createMenu(arr) {
+  const menu = document.createElement('div');
+  const list = document.createElement('ul');
+
 //   Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
 // Add those items to the <ul>
-
+  menuItems.forEach(element => {
+    const items = document.createElement('li');
+    items.textContent = element;
+    list.appendChild(items);
+  });
 // Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
 
+  menu.appendChild(list);
+
+  menu.classList.add('menu');
+
 // Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+  const button = document.querySelector(".menu-button");
 
+  button.addEventListener("click", () => {
+    menu.classList.toggle('menu--open');
+    console.log("clicked");
+});
 // Step 5: return the menu component.
-
+  return menu;
+}
 // Step 6: add the menu component to the DOM.
 
+// const header = document.querySelector(".header").appendChild(createMenu(menuItems));
+const display = document.querySelector('.header').appendChild(createMenu(menuItems));
 
 
 
